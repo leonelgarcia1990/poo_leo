8f6821e7d28fb516110dc6d0112070b9de4209ff
leonelgarcia1990 <leogarcia@uade.edu.ar>
2026-07-01 18:19:36 -0300

    refactor: remove unused cerrar method from JPAUtil class for cleaner code

--- src/com/uade/carreras/config/JPAUtil.java
+++ src/com/uade/carreras/config/JPAUtil.java
@@ -24,10 +24,6 @@ public class JPAUtil {
         return entityManagerFactory.createEntityManager();
     }
 
-    public void cerrar() {
-            if (entityManagerFactory.isOpen()) {
-                entityManagerFactory.close();
-            }
-        }
+  
 
 }
